5420d2b55b4494f51b63db435dac34153caf7f25
Mac <mac@MacdeMacBook-Pro.local>
2020-08-15 14:32:44 +0800

    完善配置

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <swiper>
+      <swiper-item v-for="(item,index) in list" :key="index">
+        <a>
+          <img :src="item"/>
+        </a>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import {Swiper, SwiperItem} from './components/swiper/index'
 export default {
   name: 'App',
+   data() {
+    return {
+      list: [
+        "https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1597280683&di=c77ecd04e118e29372cd14b2645d67b9&src=http://a3.att.hudong.com/14/75/01300000164186121366756803686.jpg",
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597290767148&di=3c9c7196a8a3ff97d59ea44b6a3465f9&imgtype=0&src=http%3A%2F%2Fa0.att.hudong.com%2F56%2F12%2F01300000164151121576126282411.jpg",
+        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1597290767148&di=897a75b037b7a64c3202be22110a8832&imgtype=0&src=http%3A%2F%2Fp2.so.qhimgs1.com%2Ft01dfcbc38578dac4c2.jpg",
+      ],
+    };
+  },
   components: {
-    HelloWorld
-  }
+    Swiper,
+    SwiperItem
+  },
+  
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
